0393dd521c6eb59fc9beed7d5c3353ff826196d3
César Gallego Rodríguez <gallego.cesar@gmail.com>
2019-01-18 13:22:11 +0100

    wip process agent request

--- tests/unit/test_webapp.py
+++ tests/unit/test_webapp.py
@@ -1,3 +1,6 @@
+import pytest
+import asyncio
+from aiohttp import web
 
 # --- objects --------------------------------------------- #
 def test_process_agent_request_exists():
@@ -11,3 +14,15 @@ def test_process_agent_request_is_a_function():
     from brainslug.webapp import process_agent_request
     assert callable(process_agent_request), "process_agent_request must be a function"
 # --- objects --------------------------------------------- #
+
+@pytest.fixture
+def cli(aiohttp_client):
+    from brainslug.webapp import process_agent_request
+    app = web.Application()
+    app.router.add_post('/next-code/{agent_code}/{machine_id}/{process_id}', process_agent_request)
+    return asyncio.get_event_loop().run_until_complete(aiohttp_client(app))
+
+async def test_process_agent_request_not_found_whitout_parameters(cli):
+    resp = await cli.post('/next-code', data={})
+    assert resp.status == 400, "must return not found if no parameters"
+
