aa985ea8a91e6ec46de735187d9f521225f945c1
Roberto Abdelkader Martínez Pérez <robertomartinezp@gmail.com>
2019-01-18 21:21:41 +0100

    Testing SyncedVar.

--- tests/unit/test_objects.py
+++ tests/unit/test_objects.py
@@ -1,6 +1,7 @@
 import inspect
 import weakref
 
+
 def test_session_exist():
     try:
         from brainslug import Session
@@ -18,8 +19,26 @@ def test_sessions_exists():
         from brainslug import SESSIONS
     except ImportError as exc:
         assert False, exc
-    
+
 
 def test_sessions_is_a_weakvaluedictionary():
     from brainslug import SESSIONS
     assert isinstance(SESSIONS, weakref.WeakValueDictionary)
+
+
+def test_syncedvar_exists():
+    try:
+        from brainslug.utils import SyncedVar
+    except ImportError as exc:
+        assert False, exc
+
+
+def test_syncedvar_is_a_class():
+    from brainslug.utils import SyncedVar
+    assert inspect.isclass(SyncedVar)
+
+
+def test_syncedvar_is_a_data_descriptor():
+    from brainslug.utils import SyncedVar
+    assert hasattr(SyncedVar, '__set__')
+    assert hasattr(SyncedVar, '__get__')
---
+++ tests/unit/test_utils.py
@@ -0,0 +1,59 @@
+import asyncio
+
+import pytest
+
+from brainslug.utils import SyncedVar
+
+
+@pytest.mark.asyncio
+async def test_syncedvar_get_is_a_coroutine(event_loop):
+    class Foo:
+        bar = SyncedVar()
+
+    foo = Foo()
+    coro = foo.bar
+
+    assert asyncio.iscoroutine(coro)
+
+    asyncio.create_task(coro).cancel()
+
+
+@pytest.mark.asyncio
+async def test_syncedvar_get_hangs(event_loop):
+    class Foo:
+        bar = SyncedVar()
+
+    foo = Foo()
+
+    with pytest.raises(asyncio.TimeoutError):
+        await asyncio.wait_for(foo.bar, 1)
+
+
+@pytest.mark.asyncio
+async def test_syncedvar_get_finish_if_set_is_called(event_loop):
+    class Foo:
+        bar = SyncedVar()
+
+    foo = Foo()
+    sentinel = object()
+
+    get_task = asyncio.create_task(foo.bar)
+    foo.bar = sentinel
+
+    assert await get_task is sentinel
+
+
+@pytest.mark.asyncio
+async def test_syncedvar_second_time_get_also_hangs(event_loop):
+    class Foo:
+        bar = SyncedVar()
+
+    foo = Foo()
+    sentinel = object()
+
+    get_task = asyncio.create_task(foo.bar)
+    foo.bar = sentinel
+    await get_task
+
+    with pytest.raises(asyncio.TimeoutError):
+        await asyncio.wait_for(foo.bar, 1)
