2723f4cefc4affefcfdbb689249730e20cd31867
Roberto Abdelkader Martínez Pérez <robertomartinezp@gmail.com>
2019-01-18 17:19:33 +0100

    Refactor Session synchronized variables using a descriptor.

--- brainslug/__init__.py
+++ brainslug/__init__.py
@@ -4,71 +4,27 @@ Contains the application runtime primitives and the global state.
 """
 import asyncio
 import weakref
+import dataclasses
 
+from brainslug.utils import SyncedVar
 
 # Global application state
 SESSIONS = weakref.WeakValueDictionary()
 
 
+@dataclasses.dataclass
 class Session:
     """
     Manage the communication channel among one agent and one or more
     zombie instances.
 
     """
-    def __init__(self, agent_type, machine_id, process_id):
-        self.agent_type = agent_type
-        self.machine_id = machine_id
-        self.process_id = process_id
+    agent_type: object
+    machine_id: object
+    process_id: object
 
-        self.__code_value = None
-        self.__code_event = asyncio.Event()
-        self.__result_value = None
-        self.__result_event = asyncio.Event()
-
-    @property
-    def _code(self):
-        """
-        Return a coroutine object who waits for the code to arrive.
-
-        .. important::
-           The returned object has to be awaited.
-
-        """
-        async def get_code():
-            """Wait for the code to arrive and return it."""
-            await self.__code_event.wait()
-            self.__code_event.clear()
-            return self.__code_value
-        return get_code()
-
-    @_code.setter
-    def _code(self, value):
-        """Store the code and flag its arrival."""
-        self.__code_value = value
-        self.__code_event.set()
-
-    @property
-    def _result(self):
-        """
-        Return a coroutine object who waits for the result to arrive.
-
-        .. important::
-           The returned object has to be awaited.
-
-        """
-        async def get_result():
-            """Wait for the result to arrive and return it."""
-            await self.__result_event.wait()
-            self.__result_event.clear()
-            return self.__result_value
-        return get_result()
-
-    @_result.setter
-    def _result(self, value):
-        """Store the result and flag its arrival."""
-        self.__result_value = value
-        self.__result_event.set()
+    _code = SyncedVar()
+    _result = SyncedVar()
 
     async def remote_eval(self, code):
         """
---
+++ brainslug/utils.py
@@ -0,0 +1,49 @@
+"""
+Miscelaneous utilities for brainslug.
+
+"""
+import asyncio
+
+
+class SyncedVar:
+    """A variable synced using an asyncio.Event."""
+    def get_value(self, instance):
+        """Return the stored value from the instance."""
+        name = f"_syncvar_{id(self)}_value"
+        if not hasattr(instance, name):
+            setattr(instance, name, None)
+        return getattr(instance, name)
+
+    def set_value(self, instance, value):
+        """Set the stored value from the instance."""
+        return setattr(instance, f"_syncvar_{id(self)}_value", value)
+
+    def get_event(self, instance):
+        """Get the Event object used for synchronization."""
+        name = f"_syncvar_{id(self)}_event"
+        if not hasattr(instance, name):
+            setattr(instance, name, asyncio.Event())
+        return getattr(instance, name)
+
+    def __get__(self, instance, owner):
+        """
+        Return a coroutine object who waits for the event before
+        returning the value.
+
+        .. important::
+           The returned value has to be awaited.
+
+        """
+        async def wait_for_value():
+            await self.get_event(instance).wait()
+            self.get_event(instance).clear()
+            return self.get_value(instance)
+        return wait_for_value()
+
+    def __set__(self, instance, value):
+        """
+        Set the value and send the event.
+
+        """
+        self.set_value(instance, value)
+        self.get_event(instance).set()
