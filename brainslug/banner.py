e5e6544fd133bd31c365f8dceef70fd67b1650dd
Roberto Abdelkader Martínez Pérez <robertomartinezp@gmail.com>
2019-01-23 18:16:39 +0100

    swag++

---
+++ brainslug/banner.py
@@ -0,0 +1,11 @@
+import pkg_resources
+
+def generate_banner():
+    distribution = pkg_resources.get_distribution('brainslug')
+    version = f'v{distribution.version}'
+    return (f"""
+  ______            __       _______ __
+ |   __ .----.---.-|__.-----|     __|  .--.--.-----.
+ |   __ |   _|  _  |  |     |__     |  |  |  |  _  |
+ |______|__| |___._|__|__|__|_______|__|_____|___  |"""
+                        f"\n{version: >42}   |_____|")
