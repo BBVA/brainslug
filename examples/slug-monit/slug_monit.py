d7ffd44a6cc2dbf9c731137fd6cffc16c4eab6de
César Gallego Rodríguez <cesar.gallego.next@bbva.com>
2019-01-25 12:47:33 +0100

    wip: live server reload on monit

--- examples/slug-monit/slug_monit.py
+++ examples/slug-monit/slug_monit.py
@@ -1,21 +1,32 @@
 import time
+import threading
 
 from brainslug import slug, run_locally, Brain
 import psutil
+from livereload import Server
+
+
+def start_server():
+    s = Server()
+    s.watch("*.json")
+    s.serve()
+
 
 @slug(remote=Brain.platform == 'linux')
 def slug_monit(remote):
+    t = threading.Thread(target=start_server())
+    t.run()
     while True:
         info = {
-                "cpu": psutil.cpu_percent(interval=1),
-                "mem": psutil.virtual_memory().percent,
-                "disk": psutil.disk_usage("/").percent,
-                "net_in": psutil.net_io_counters().bytes_recv,
-                "net_out": psutil.net_io_counters().bytes_sent,
+                "cpu": remote.psutil.cpu_percent(interval=1),
+                "mem": remote.psutil.virtual_memory().percent,
+                "disk": remote.psutil.disk_usage("/").percent,
+                "net_in": remote.psutil.net_io_counters().bytes_recv,
+                "net_out": remote.psutil.net_io_counters().bytes_sent,
                 "totals":{
-                        "mem": psutil.virtual_memory().total,
-                        "disk": psutil.disk_usage("/").total,
-                        "uptime": psutil.boot_time()
+                        "mem": remote.psutil.virtual_memory().total,
+                        "disk": remote.psutil.disk_usage("/").total,
+                        "uptime": remote.psutil.boot_time()
                     }
                 }
         print(info)
