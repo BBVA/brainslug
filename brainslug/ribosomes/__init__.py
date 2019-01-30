86c2503f815273677868794721c26e53934de76a
Roberto Abdelkader Martínez Pérez <robertomartinezp@gmail.com>
2019-01-30 11:39:03 +0100

    Symbol does not allow dunder methods to be defined.

--- brainslug/ribosomes/__init__.py
+++ brainslug/ribosomes/__init__.py
@@ -3,7 +3,9 @@ RIBOSOMES = dict()
 
 class Symbol(tuple):
     def __getattribute__(self, name):
-        if name.isidentifier():
+        if name.startswith('__') and name.endswith('__'):
+            return super().__getattribute__(name)
+        elif name.isidentifier():
             return Symbol(self + (name, ))
         else:
             raise AttributeError('Invalid attribute %r' % name)
--- tests/unit/test_symbol.py
+++ tests/unit/test_symbol.py
@@ -29,10 +29,26 @@ def test_symbol_invalid_attribute(attribute):
         getattr(Symbol(), attribute)
 
 
+@given(attribute=st.text(min_size=1,
+                         alphabet=ascii_letters + digits + '_'))
+def test_symbol_dunders_are_forbidden(attribute):
+    assume(attribute.isidentifier())
+    s = Symbol()
+    try:
+        super(Symbol, s).__getattribute__(f'__{attribute}__')
+    except AttributeError:
+        with pytest.raises(AttributeError):
+            getattr(s, f'__{attribute}__')
+    else:
+        assume(False)
+
+
 @given(path=st.tuples(st.text(min_size=1,
                               alphabet=ascii_letters + digits + '_')))
 def test_symbol_is_aggregative(path):
-    assume(not any(p[0] in digits for p in path))
+    assume(not any(p[0] in digits
+                   and not (p.startswith('__') and p.endswith('__'))
+                   for p in path))
     result = Symbol()
     for part in path:
         result = getattr(result, part)
