020091c17a9660bd1a864aeca2eee7b831d9507b
Roberto Abdelkader Martínez Pérez <robertomartinezp@gmail.com>
2019-01-30 18:04:53 +0100

    Refactor.

--- brainslug/ribosomes/__init__.py
+++ brainslug/ribosomes/__init__.py
@@ -2,17 +2,18 @@ RIBOSOMES = dict()
 
 
 def define(symbol):
-    def _decorated(fn):
+    def _register(fn):
         def is_dunder(s):
             return s.startswith("__") and s.endswith("__")
+
         if any(is_dunder(part) for part in symbol):
             raise ValueError("Cannot define double underscore methods")
         elif symbol in RIBOSOMES:
             raise ValueError("Cannot define ribosome twice")
         else:
-            RIBOSOMES[symbol] = fn
-            return fn
-    return _decorated
+            return RIBOSOMES.setdefault(symbol, fn)
+
+    return _register
 
 
 class Symbol(tuple):
