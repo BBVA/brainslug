73ba664e7ccee714fced70aaab6dbe90cfb7b426
Roberto Abdelkader Martínez Pérez <robertomartinezp@gmail.com>
2019-03-06 17:15:08 +0100

    Including readme and trove classifiers.

--- setup.py
+++ setup.py
@@ -1,11 +1,18 @@
 from setuptools import setup, find_packages
 
-VERSION = '0.0.1'
+VERSION = '0.0.2'
 
 setup(name='brainslug',
       version=VERSION,
       description="Parasitic Computing Framework",
+      long_description=open('README.rst', 'r').read(),
       classifiers=[
+          "Development Status :: 3 - Alpha",
+          "Intended Audience :: Developers",
+          "License :: OSI Approved :: Apache Software License",
+          "Topic :: Internet",
+          "Topic :: Security",
+          "Topic :: Software Development :: Libraries :: Application Frameworks",
       ],
       keywords='parasitic computing framework',
       packages=find_packages(exclude=["tests", "docs", "poc"]),
